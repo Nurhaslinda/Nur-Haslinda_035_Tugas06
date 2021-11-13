8df8a83d01aa76e03ca4bc76ddc2287bfcd3af6d
Nurhaslinda <89765164+Nurhaslinda@users.noreply.github.com>
2021-11-13 17:09:21 +0700

    Add files via upload

---
+++ 035_Nur Haslinda_Permasalahan 23.cpp
@@ -0,0 +1,18 @@
+/*Program dua puluh empat*/
+/*Program sederhana menggunakan fungsi*/
+/*yang tidak mengambil nilai*/
+#include <stdio.h>
+#include <conio.h>
+
+void garis();	/*deklarasi fungsi*/
+main()
+{
+	garis();
+	printf("NO  NIM  NAMA  NILAI\n");
+	garis();
+	getch();
+}
+void garis()	/*definisi fungsi*/
+{
+	printf("==========================\n");
+}
